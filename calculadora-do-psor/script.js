1511badcad879110b96f262490ff3b4c889591a3
victormatozo <163305123+victormatozo@users.noreply.github.com>
2024-09-23 08:30:04 -0300

    a
    a

---
+++ calculadora-do-psor/script.js
@@ -0,0 +1,47 @@
+let currentInput = '' ;
+let operation = '' ;
+let previousInput = '';
+
+function appendNumber (number)  {
+    currentInput += number;
+    updateDisplay();
+}
+
+function setOperation (op) {
+    if (currentInput === '') return;
+    if (previousInput !== '') {
+        calculate();
+    }
+    operation = op;
+    previousInput = currentInput;
+    currentInput = '';
+}
+
+function calculate() {
+    let result;
+    const prev = parseFloat(previousInput);
+    const current = parseFloat(currentInput);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (operation) {
+        case '+': result = prev + current; break;
+        case '': result = prevcurrent; break;
+        case '*': result = prevcurrent; break;
+        case '/': result = prev / current; break;
+        default: return;
+    }
+    currentInput = result;
+    operation = '';
+    previousInput = '';
+    updateDisplay();
+}
+
+function clearDisplay() {
+    currentInput = '';
+    operation = '';
+    previousInput = '';
+    updateDisplay();
+    }
+
+    function updateDisplay() {
+        document.getElementById('display').value = currentInput;
+    }
